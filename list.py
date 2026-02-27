35a978a232f0fee5cdcc55a064a73122090c8493
areeba-cyber <arebareeba409@gmail.com>
2026-02-27 06:16:28 +0500

    List

---
+++ list.py
@@ -0,0 +1,6 @@
+# List also called ARRAY in many other languages
+
+tea_varieties = ["White", "Black", "Green","Oolong"]
+print(tea_varieties.pop())
+print(tea_varieties.remove("White"))
+print(tea_varieties.insert(1:"Yellow"))
