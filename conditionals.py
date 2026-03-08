11b2a9ccda05d2622ea3974d40fadeb7bf109f9c
areeba-cyber <arebareeba409@gmail.com>
2026-03-08 12:47:35 +0500

    Solution 2

--- conditionals.py
+++ conditionals.py
@@ -14,8 +14,20 @@ else
 
 2. Movie Ticket Pricing
 Problem: Movie tickets are priced based on age: $12 for adults (18 and over), $8 for children. Everyone gets a $2 discount on Wednesday.
+# Solution
+
+age = 26
+day = "Wednesday"
+price = 12 if age>=18 else 8
+if day="Wednesday":
+price-=2
+print("Ticket price for you is $", price)
+
 3. Grade Calculator
 Problem: Assign a letter grade based on a student's score: A (90-100), B (80-89), C (70-79), D (60-69), F (below 60).
+# Solution
+
+
 4. Fruit Ripeness Checker
 Problem: Determine if a fruit is ripe, overripe, or unripe based on its color. (e.g., Banana: Green - Unripe, Yellow - Ripe, Brown - Overripe)
 5. Weather Activity Suggestion
