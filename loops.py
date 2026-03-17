f0cf34196afdf4b6f54aa60b6df2812931df32c1
areeba-cyber <arebareeba409@gmail.com>
2026-03-17 21:27:55 +0500

    Solution 1

--- loops.py
+++ loops.py
@@ -2,6 +2,15 @@ Loops in Python
 1. Counting Positive Numbers
 Problem: Given a list of numbers, count how many are positive.
 numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+# Solution
+
+numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+positive_number_count = 0
+for num in numbers:
+    if num > 0:
+        positive_number_count += 1
+print("Final count of Positive number is: ", positive_number_count)
+
 2. Sum of Even Numbers
 Problem: Calculate the sum of even numbers up to a given number n.
 3. Multiplication Table Printer
