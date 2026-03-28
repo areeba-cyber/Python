22ef511409fed9cc39aa25c0912db3d03f2bba48
areeba-cyber <arebareeba409@gmail.com>
2026-03-28 19:50:49 +0500

    Solution

---
+++ Exercise.py
@@ -0,0 +1,17 @@
+# Exercise: Python Variables
+# 1. Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
+
+# break = 5
+# Break is a reserved keyword (used in for and while loop to exit a loop) in python not a variable name Thats why it is showing error
+
+# 2. Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+
+X = 2003
+Y = 2026
+age = Y - X
+print("My age is: ", age)
+
+
+# 3. Store your first, middle and last name in three different variables and then print your full name using these variables
+
+# 4. Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
