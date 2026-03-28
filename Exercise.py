c08ca82f919785f1cebb4d56b2d736ec0617b7cc
areeba-cyber <arebareeba409@gmail.com>
2026-03-28 20:57:13 +0500

    Solution Strings

--- Exercise.py
+++ Exercise.py
@@ -65,5 +65,19 @@ print("Address in f format is:",  address2)
 # 2. Create a variable to store the string "Earth revolves around the sun"
 # Print "revolves" using slice operator
 # Print "sun" using negative index
+
+string = "Earth revolves around the sun"
+print(string[6:14])
+print(string[-3:])
+
 # 3. Create two variables to store how many fruits and vegetables you eat in a day. Now Print "I eat x veggies and y fruits daily" where x and y presents vegetables and fruits that you eat everyday. Use python f string for this.
+
+num_fruits=10
+num_veggies=5
+print(f"I eat {num_veggies} veggies and {num_fruits} daily")
+
 # 4. I have a string variable called s='maine 200 banana khaye'. This of course is a wrong statement, the correct statement is 'maine 10 samosa khaye'. Replace incorrect words in original strong with new ones and print the new string. Also try to do this in one line.
+
+s='maine 200 banana khaye'
+r = s.replace('200','10').replace('banana','samosa')
+print("Replacment in one line:", r)
