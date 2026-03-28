a362c72f462b4318e3f059c542148d38a7890194
areeba-cyber <arebareeba409@gmail.com>
2026-03-28 20:28:08 +0500

    SOLution

--- Exercise.py
+++ Exercise.py
@@ -24,6 +24,20 @@ print("My name is: " +First+ " " +Middle+ " " + Last)
 
 # Numbers in python
 # 1. You have a football field that is 92 meter long and 48.8 meter wide. Find out total area using python and print it.
+
+L = 92
+w = 48.8
+Area = L * w
+print("Area is :", Area)
+
 # 2. You bought 9 packets of potato chips from a store. Each packet costs 1.49 dollar and you gave shopkeeper 20 dollar. Find out using python, how many dollars is the shopkeeper going to give you back?
+
+num_packets = 9
+cost_per_packet = 1.49
+total_cost = num_packets*cost_per_packet
+money_paid = 20
+money_back = money_paid - total_cost
+print("Cash back:", money_back)
+
 # 3. You want to replace tiles in your bathroom which is exactly square and 5.5 feet is its length. If tiles cost 500 rs per square feet, how much will be the total cost to replace all tiles. Calculate and print the cost using python (Hint: Use power operator ** to find area of a square)
 # 4. Print binary representation of number 17
