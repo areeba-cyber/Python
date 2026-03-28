543a52e33babcacd4999e6326d0c67f7e2064fd9
areeba-cyber <arebareeba409@gmail.com>
2026-03-28 20:00:45 +0500

    Numbers

--- Exercise.py
+++ Exercise.py
@@ -21,3 +21,9 @@ print("My name is: " +First+ " " +Middle+ " " + Last)
 
 # 4. Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
 # 1record, record-one, record^one, continue
+
+# Numbers in python
+# 1. You have a football field that is 92 meter long and 48.8 meter wide. Find out total area using python and print it.
+# 2. You bought 9 packets of potato chips from a store. Each packet costs 1.49 dollar and you gave shopkeeper 20 dollar. Find out using python, how many dollars is the shopkeeper going to give you back?
+# 3. You want to replace tiles in your bathroom which is exactly square and 5.5 feet is its length. If tiles cost 500 rs per square feet, how much will be the total cost to replace all tiles. Calculate and print the cost using python (Hint: Use power operator ** to find area of a square)
+# 4. Print binary representation of number 17
