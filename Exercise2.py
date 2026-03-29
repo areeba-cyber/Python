474b14b760b932c683674296c0ff522675a567dc
areeba-cyber <arebareeba409@gmail.com>
2026-03-29 06:50:43 +0500

    Solution

--- Exercise2.py
+++ Exercise2.py
@@ -6,14 +6,27 @@
 # April - 2130
 # May - 2190
 # Create a list to store these monthly expenses and using that find out,
-
+exp = [2200, 2350, 2600, 2130, 2190]
 # 1. In Feb, how many dollars you spent extra compare to January?
+print("In feb I spent ",exp[1]-exp[0],"extra expenses" )
+
 # 2. Find out your total expense in first quarter (first three months) of the year.
+print("The total expense in first quater is:", exp[0]+exp[1]+exp[2])
+
 # 3. Find out if you spent exactly 2000 dollars in any month
+print("Did I spent 2000$ in any month?", 2000 in exp)
+
 # 4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
+print(exp + [1980])
+
+exp.append(1980)
+print("The expenses at the end of june is:", exp)
+
 # 5. You returned an item that you bought in a month of April and
 # got a refund of 200$. Make a correction to your monthly expense list
 # based on this
+exp[3] = exp[3] - 200
+print("Expenses after 200$ return in April:",exp)
 
 
 
