631912e302d2d6d2e58835d04ce0f2911e194896
areeba-cyber <arebareeba409@gmail.com>
2026-03-30 14:35:55 +0500

    Dict

--- Exercise4.py
+++ Exercise4.py
@@ -6,16 +6,28 @@
 # India	    136
 # USA	    32
 # Pakistan	21
+
 # Using above create a dictionary of countries and its population
+
+population = {
+    'china' : 143,
+    'india' : 136,
+    'usa' :32,
+    'pakistan' : 21
+}
+
 # Write a program that asks user for three type of inputs,
-# print: if user enter print then it should print all countries with their population in this format,
+# (a) print: if user enter print then it should print all countries with their population in this format,
 # china==>143
 # india==>136
 # usa==>32
 # pakistan==>21
-# add: if user input add then it should further ask for a country name to add. If country already exist in our dataset then it should print that it exist and do nothing. If it doesn't then it asks for population and add that new country/population in our dictionary and print it
-# remove: when user inputs remove it should ask for a country to remove. If country exist in our dictionary then remove it and print new dictionary using format shown above in (a). Else print that country doesn't exist!
-# query: on this again ask user for which country he or she wants to query. When user inputs that country it will print population of that country.
+
+
+
+# (B) add: if user input add then it should further ask for a country name to add. If country already exist in our dataset then it should print that it exist and do nothing. If it doesn't then it asks for population and add that new country/population in our dictionary and print it
+# (C) remove: when user inputs remove it should ask for a country to remove. If country exist in our dictionary then remove it and print new dictionary using format shown above in (a). Else print that country doesn't exist!
+# (D) query: on this again ask user for which country he or she wants to query. When user inputs that country it will print population of that country.
 # Solution
 
 # 2. You are given following list of stocks and their prices in last 3 days,
