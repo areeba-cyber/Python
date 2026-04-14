8f35076b3d5c3ce43397e537a9d740214d31a16e
areeba-cyber <arebareeba409@gmail.com>
2026-04-14 07:48:28 +0500

    List comprehension

--- comprehension.py
+++ comprehension.py
@@ -1,1 +1,8 @@
-# Used to enhace the code
+# Used to enhace the code, write the code in best possible way,
+# Increase performance, decrease the size of code
+
+marks = [10,20,30,40,50]
+new_marks = []
+for x in marks:
+    new_marks.append(x+2)
+print(new_marks)
